8f8ebf6da8d8d35549c4ac41dc9e9be76cc38a67
Emircan <emircandv787@gmail.com>
2024-03-01 00:09:23 +0300

    add singleton

---
+++ DesignPatterns.Singleton/CountryProvider.cs
@@ -0,0 +1,34 @@
+﻿using DesignPatterns.Singleton.Models;
+
+namespace DesignPatterns.Singleton
+{
+    public class CountryProvider
+    {
+        public List<Country> Coutries { get; private set; }
+
+        public static CountryProvider instance;
+        public static CountryProvider Instance => instance ??= new CountryProvider();
+        private CountryProvider()
+        {
+            if (Coutries is null)
+            {
+                Task.Delay(2000).GetAwaiter().GetResult();
+
+                Coutries = new List<Country>()
+                {
+                     new Country()
+                     {
+                         Name ="Türkiye"
+                     },
+                     new Country()
+                     {
+                         Name = "Birleşik Krallık"
+                     }
+                };
+            }
+
+        }
+        public async Task<List<Country>> GetCountries() => Coutries;
+
+    }
+}
---
+++ DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace DesignPatterns.Singleton
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+
+
+
+            var countries = await CountryProvider.Instance.GetCountries();
+
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+            foreach (var country in countries)
+            {
+                Console.WriteLine(country.Name);
+            }
+
+
+            var countries1 = await CountryProvider.Instance.GetCountries();
+
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+
+        }
+    }
+}
